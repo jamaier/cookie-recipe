8d869388b1f628f82882fe19a984fd80dcfcd48d
Jacob Maier <jacobamaier@gmail.com>
2023-01-11 08:13:10 -0800

    Added proper onclicks following the information instead of addeventlistener.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
-document.querySelector('.switch-light').addEventListener('click', function() {
-  document.body.style.backgroundColor = 'white';
-});
+document.querySelector(".switch-dark").onclick = function() {
+  document.body.style.backgroundColor = "black";
+  document.body.style.color = "white";
+};
 
-document.querySelector('.switch-dark').addEventListener('click', function() {
-  document.body.style.backgroundColor = 'gray';
-});
+document.querySelector(".switch-light").onclick = function() {
+  document.body.style.backgroundColor = "white";
+  document.body.style.color = "black";
+};
+
+// document.querySelector('.switch-light').addEventListener('click', function() {
+//   document.body.style.backgroundColor = 'white';
+// });
+
+// document.querySelector('.switch-dark').addEventListener('click', function() {
+//   document.body.style.backgroundColor = 'gray';
+// });
